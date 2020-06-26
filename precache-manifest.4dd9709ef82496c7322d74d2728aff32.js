fec7f07308939a476176e3df6ad8b60af329510d
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-06-26 18:57:23 -0400

    Updates

---
+++ precache-manifest.4dd9709ef82496c7322d74d2728aff32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8362cf256318401c87640e29a34833a",
+    "url": "/twitch-clone/index.html"
+  },
+  {
+    "revision": "3c92cc95a60bc7d43f00",
+    "url": "/twitch-clone/static/css/main.1861cfef.chunk.css"
+  },
+  {
+    "revision": "88a317adecfcd4be3b27",
+    "url": "/twitch-clone/static/js/2.3dcda9d3.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/twitch-clone/static/js/2.3dcda9d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c92cc95a60bc7d43f00",
+    "url": "/twitch-clone/static/js/main.2dd07af2.chunk.js"
+  },
+  {
+    "revision": "15d48a89c8b15bb98fa6",
+    "url": "/twitch-clone/static/js/runtime-main.60ca3412.js"
+  }
+]);
